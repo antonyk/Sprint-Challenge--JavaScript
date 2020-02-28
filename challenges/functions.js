bef347922ef9c26fa00ea4cc34f1b374557254d2
Anthony K <antonyk@users.noreply.github.com>
2020-02-28 11:56:32 -0500

    Completed functions portion

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,29 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, cb) {
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add(a, b) {
+  return a + b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,6 +37,8 @@
 
 // Explanation: 
 
+// When nestedFunction() is invoked, a closure is created which includes all of the lexical scope (context) of the function's declaration.
+// The variable "internal" is part of that scope (outside function scope) and is therefore included in the closure and thus available to nestedFunction during invocation.
 
 const external = "I'm outside the function";
 
